c408ea81b4f69edf55690531c1dfe3e2074996be
hsn6 <hsn6@openmailbox.org>
2017-03-06 06:38:52 +0330

    forigxi Tu tage

---
+++ .openshift/cron/daily/run_python.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source $OPENSHIFT_HOMEDIR/python/virtenv/venv/bin/activate
+python3 $OPENSHIFT_REPO_DIR/wsgi/tage.py
--- wsgi/tage.py
+++ wsgi/tage.py
@@ -2,23 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from peewee import *
-from application import Uzanto, Parto, UzantoVidPunkto, db
+from application import Uzanto, Tu, db
 
 db.connect()
 
-Uzanto.update(mono=Uzanto.mono+7).where(Uzanto.id != 1).execute()
-
-partoj = Parto.select().where((Parto.minajxo > 0) & (Parto.uzanto != 1))
-for parto in partoj:
-    if parto.materialo == 'argxento':
-        Uzanto.update(mono=Uzanto.mono+parto.nivelo).where(Uzanto.id == parto.uzanto.id).execute()
-    elif parto.materialo == 'oro':
-        Uzanto.update(oro=Uzanto.oro+parto.nivelo).where(Uzanto.id == parto.uzanto.id).execute()
-    parto.minajxo -= parto.nivelo
-    if parto.minajxo < 0:
-        parto.minajxo = 0
-    parto.save()
-
-Uzanto.update(oro=40).where(Uzanto.oro > 40).execute()
+Tu.delete().execute()
 
 db.close()
